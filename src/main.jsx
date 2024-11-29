8c98b73240671cf35c43eafcba8b84fb27d262ad
cabrera <edancabreraav@ittepic.edu.mx>
2024-11-29 12:14:07 -0700

    Más código inncesesario

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-
 import './share/css/allPages.css'
 import AppAllModules from './AppAllModules'
 
-import { Provider } from 'react-redux'
-import store from './ecommerce/redux/store/store'
-
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Provider store={store}>
       <AppAllModules />
-    </Provider>
   </StrictMode>,
 )
